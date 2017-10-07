7fd375dde154845ebd37cdee38c9826f3b3d6788
Daveu1983 <31164433+Daveu1983@users.noreply.github.com>
2017-10-07 09:00:47 +0100

    Improve code
    Put answers in array rather than using if statements, put size of array in random function so answers can be added or removed without having to change this part of the code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace MagicEight
 {
@@ -27,71 +27,16 @@ namespace MagicEight
 
 	class AnswerQuestion
 	{
-		private string Answer;
-		public void getAnswer(){
+		private string[] Answer = {"It is certain","It is decidely so","Without a doubt", "Yes definately",
+			"You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes",
+			"Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now",
+			"Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", 
+			"Outlook not so good", "Very doubtful"};
+		public void getAnswer(){ 
 			Random r = new Random();
-			int ran = r.Next(1,20);
-			if (ran == 1) {
-				Answer = "It is certain";
-			}
-			if (ran == 2) {
-				Answer = "It is decidely so";
-			}
-			if (ran == 3) {
-				Answer = "Without a doubt";
-			}
-			if (ran == 4) {
-				Answer = "Yes definately";
-			}
-			if (ran == 5) {
-				Answer = "You may rely on it";
-			}
-			if (ran == 6) {
-				Answer = "As I see it, yes";
-			}
-			if (ran == 7) {
-				Answer = "Most likely";
-			}
-			if (ran == 8) {
-				Answer = "Outlook good";
-			}
-			if (ran == 9) {
-				Answer = "Yes";
-			}
-			if (ran == 10) {
-				Answer = "Signs point to yes";
-			}
-			if (ran == 11) {
-				Answer = "Reply hazy try again";
-			}
-			if (ran == 12) {
-				Answer = "Ask again later";
-			}
-			if (ran == 13) {
-				Answer = "Better not tell you now";
-			}
-			if (ran == 14) {
-				Answer = "Cannot predict now";
-			}
-			if (ran == 15) {
-				Answer = "Concentrate and ask again";
-			}
-			if (ran == 16) {
-				Answer = "Don't count on it";
-			}
-			if (ran == 17) {
-				Answer = "My reply is no";
-			}
-			if (ran == 18) {
-				Answer = "My sources say no";
-			}
-			if (ran == 19) {
-				Answer = "Outlook not so good";
-			}
-			if (ran == 20) {
-				Answer = "Very doubtful";
-			}
-			Console.WriteLine("\n{0}", Answer);
+			int answerLength = Answer.Length;
+			int ran = r.Next(1, answerLength);
+			Console.WriteLine("\n{0}", Answer[ran]);
 		}
 	}
 }
